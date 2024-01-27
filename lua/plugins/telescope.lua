3652eee066fd8653eb0fdefc40b3439c333a982c
Ascendise <rapperido@gmail.com>
2024-01-28 00:56:38 +0100

    Add shortcut for Telescope Treesitter

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,19 @@
 return {
-    'nvim-telescope/telescope.nvim', 
+    'nvim-telescope/telescope.nvim',
 	tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
+		require('telescope').setup({
+			pickers = {
+				find_files = {
+					hidden = true
+				}
+			}
+		})
 		local telescope = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>ff', telescope.find_files, {})
 		vim.keymap.set('n', '<leader>fg', telescope.live_grep, {})
+		vim.keymap.set('n', '<leader>ft', telescope.treesitter, {})
 		vim.keymap.set('n', '<leader>fb', telescope.buffers, {})
 		vim.keymap.set('n', '<leader>fh', telescope.help_tags, {})
 		vim.keymap.set('n', '<leader>fcs', telescope.colorscheme, {})
